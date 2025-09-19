6ae92e2ea3721091da7521c5e82ee179b7c660d1
Erick Vinicius Rodrigues <erickafram08@gmail.com>
2025-09-19 15:09:09 -0300

    Corrigir verificação automática de status do pagamento PIX

--- public/js/portal.js
+++ public/js/portal.js
@@ -7,6 +7,7 @@ class WiFiPortal {
     constructor() {
         this.deviceMac = '';
         this.connectionCheckInterval = null;
+        this.paymentCheckInterval = null;
         this.loadingOverlay = null;
         this.paymentModal = null;
         this.registrationModal = null;
